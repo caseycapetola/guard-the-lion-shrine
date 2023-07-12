e420090f1e8447f476b271816f3677dc233acd83
Casey Capetola <caseycapetola@gmail.com>
2023-07-12 16:35:00 -0400

    Changed navbar color and added Pozniak svg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-[#ED70C0] fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" className="flex items-center">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="/poz_dark_adobe_express.svg"
             className="h-8 mr-3"
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Guard the Lion Shrine
           </span>
         </a>
         <div className="flex md:order-2">
-          <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Get started
-          </button>
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
