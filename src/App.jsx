ca5d2a839524bc5c9d7464197e03b0b88591d3ee
Casey Capetola <caseycapetola@gmail.com>
2023-07-12 16:21:48 -0400

    Added basic Navbar config

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
   },
-  {
-    path: "/about",
-    element: <AboutPage />,
-  },
-  {
-    path: "/schedule",
-    element: <SchedulePage />,
-  },
+  // {
+  //   path: "/about",
+  //   element: <AboutPage />,
+  // },
+  // {
+  //   path: "/schedule",
+  //   element: <SchedulePage />,
+  // },
 ]);
 
 function App() {
