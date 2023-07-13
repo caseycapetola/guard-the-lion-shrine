87fe9dda63aab529c6383b218631c770fca92569
Casey Capetola <caseycapetola@gmail.com>
2023-07-12 22:46:53 -0400

    Added basic framework for embedded video

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -11,34 +10,16 @@ import AboutPage from "./pages/AboutPage";
 import SchedulePage from "./pages/SchedulePage";
 import MiscPage from "./pages/MiscPage";
 import Navbar from "./components/Navbar";
-
-// export const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <LandingPage />,
-//   },
-//   {
-//     path: "/about",
-//     element: <AboutPage />,
-//   },
-//   // {
-//   //   path: "/schedule",
-//   //   element: <SchedulePage />,
-//   // },
-// ]);
-
 function App() {
   return (
     <>
       <Navbar />
-      <div className="text-center">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/schedule" element={<SchedulePage />} />
-          <Route path="/misc" element={<MiscPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/schedule" element={<SchedulePage />} />
+        <Route path="/misc" element={<MiscPage />} />
+      </Routes>
     </>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,39 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+import React, { useState } from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Container } from "react-bootstrap";
 
 const LandingPage = () => {
   return (
     <>
-      <p>Landing Page</p>
+      <div className="bg-black text-white">
+        <div className="flex flex-col flex-wrap text-center content-stretch">
+          <div className="bg-red-500 flex-auto">
+            What is Guard the Lion Shrine?
+          </div>
+          <div className="bg-blue-500 flex flex-grow">
+            <iframe
+              className="w-full aspect-[9/16] p-16"
+              title="Youtube player"
+              sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+              src={"https://www.youtube.com/embed/FCxXHcTiDLY"}
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="">
+            <p>Testing</p>
+          </div>
+        </div>
+      </div>
+
+      {/* <Container>
+        <div className="ratio ratio-16x9">
+          <iframe
+            src="https://www.youtube.com/embed/FCxXHcTiDLY"
+            title="GTLS Video"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </Container> */}
     </>
   );
 };
