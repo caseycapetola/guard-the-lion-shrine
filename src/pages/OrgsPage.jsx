61942fff0ee78e6ce1208bd007637e9f80334b4c
Casey Capetola <caseycapetola@gmail.com>
2023-07-14 15:44:24 -0400

    Added header text for OrgsPage

--- src/pages/OrgsPage.jsx
+++ src/pages/OrgsPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "animate.css";
+import Footer from "../components/Footer";
 
-const MiscPage = () => {
-  return <div>MiscPage</div>;
+const OrgsPage = () => {
+  return (
+    <div className="bg-black text-white">
+      <div className="flex flex-col flex-wrap text-center content-stretch">
+        <div className="animate__animated animate__fadeInUp">
+          <div className="flex-auto text-4xl pt-16">Organizations</div>
+          <div className="p-16">
+            <p className="text-2xl">
+              Lion Ambassadors would like to thank the following organizations
+              for their support in Guard the Lion Shrine.
+            </p>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
-export default MiscPage;
+export default OrgsPage;
