26e6ad0b7fa5e4b33b576294aa883b921ffa319a
Casey Capetola <caseycapetola@gmail.com>
2023-07-17 15:34:35 -0400

    Configured hamburger menu for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React, { Children } from "react";
+import { useState } from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import poz from "../assets/poz_dark_adobe_express.svg";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   const path = window.location.pathname;
+  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
   return (
     <nav className="border-black bg-[#ED70C0]">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -16,9 +19,10 @@ const Navbar = () => {
         <button
           data-collapse-toggle="navbar-solid-bg"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-solid-bg"
           aria-expanded="false"
+          onClick={() => setIsNavOpen((prev) => !prev)}
         >
           <span className="sr-only">Open main menu</span>
           <svg
@@ -37,6 +41,48 @@ const Navbar = () => {
             />
           </svg>
         </button>
+
+        <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+          <div
+            className={
+              isNavOpen
+                ? "showMenuNav animate__animated animate__fadeIn"
+                : "hideMenuNav"
+            }
+          >
+            <div
+              className="absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-white cursor-pointer"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="flex flex-col items-center justify-between min-h-[250px]">
+              <li className="my-8 uppercase">
+                <a href="/guard-the-lion-shrine">Home</a>
+              </li>
+              <li className="my-8 uppercase">
+                <a href="/guard-the-lion-shrine/about">About</a>
+              </li>
+              <li className="my-8 uppercase">
+                <a href="/guard-the-lion-shrine/schedule">Schedule</a>
+              </li>
+              <li className="my-8 uppercase">
+                <a href="/guard-the-lion-shrine/organizations">Organizations</a>
+              </li>
+            </ul>
+          </div>
+        </div>
         <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
           <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <CustomLink to="/guard-the-lion-shrine">Home</CustomLink>
